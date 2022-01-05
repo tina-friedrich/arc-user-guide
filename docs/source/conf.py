e6e4f17c3b4fa6fe724fe4370a0e8300cd7725bf
oxford-arc <66992362+oxford-arc@users.noreply.github.com>
2022-01-05 12:55:22 +0000

    Update conf.py
    Initial update for ARC documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'ARC User Guide'
+copyright = '2021'
+author = 'ARC Team'
 
 release = '0.1'
 version = '0.1.0'
