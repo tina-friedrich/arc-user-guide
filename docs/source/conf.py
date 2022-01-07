b4ea1dc1909506a59ba4d5c611f3b3beff917502
oxford-arc <66992362+oxford-arc@users.noreply.github.com>
2022-01-07 08:36:06 +0000

    Update conf.py
    Added _static directory for css

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,6 +34,14 @@ html_theme = 'sphinx_rtd_theme'
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
 
-# -- Add ARC theme overrides
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
+# -- Add ARC theme overrides...
+# This will be found in source/_static (as defined above)
+
 def setup(app):
     app.add_css_file('arc_theme.css')
